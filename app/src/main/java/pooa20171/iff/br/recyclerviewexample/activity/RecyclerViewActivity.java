86d9d43b77a6be07bae0369ab94acae9e64aab7b
Luiz Gustavo <lglmoura@gmail.com>
2017-06-05 17:05:30 -0300

    Alterando o Activity Principal

--- app/src/main/java/pooa20171/iff/br/recyclerviewexample/activity/RecyclerViewActivity.java
+++ app/src/main/java/pooa20171/iff/br/recyclerviewexample/activity/RecyclerViewActivity.java
@@ -1,9 +1,16 @@
 package pooa20171.iff.br.recyclerviewexample.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import pooa20171.iff.br.recyclerviewexample.R;
+import pooa20171.iff.br.recyclerviewexample.adapter.LivroAdapter;
+import pooa20171.iff.br.recyclerviewexample.model.Livro;
 
 public class RecyclerViewActivity extends AppCompatActivity {
 
@@ -11,5 +18,26 @@ public class RecyclerViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_Livro);
+
+        recyclerView.setAdapter(new LivroAdapter(getLivros(),this));
+        RecyclerView.LayoutManager layout = new LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL, false);
+
+        recyclerView.setLayoutManager(layout);
+
+    }
+
+    private List<Livro> getLivros(){
+        List<Livro> livros = new ArrayList<Livro>();
+        int i =0;
+        for(i=0;i<=20;i++){
+            String iv = String.valueOf(i);
+            Livro livro = new Livro("livro".concat(iv),"Autor".concat(iv),"Descricao".concat(iv));
+            livros.add(livro);
+        }
+        return livros;
+
     }
 }
